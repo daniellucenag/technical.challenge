8c80be7d6a5a84b9cb64e25719e0ac34c8bac5ee
Daniel Lucena <daniel.lucena@bs2.com>
2020-09-25 15:03:38 -0300

    Add project files.

---
+++ src/technical.challenge.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace technical.challenge.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
