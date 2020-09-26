a68a4c54d0cb89c8e42bc345204e9f7a2df88e61
Daniel Lucena <daniel.lucena@bs2.com>
2020-09-26 13:19:42 -0300

    Entrada solicitando numero do usuario

--- src/technical.challenge.console/Program.cs
+++ src/technical.challenge.console/Program.cs
@@ -1,21 +1,43 @@
 ﻿namespace technical.challenge.console
 {
     using System;
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {  
-            int numero = 12;
-            Console.WriteLine($"Divisores do numero {numero}!");
+        public static void Main(string[] args)
+        {              
 
-            for (int i = 1; i <= numero; i++)
+            while (true)
             {
-                //verificar se numero é divisor
-                if (numero % i == 0)
-                {   
-                    Console.WriteLine($"{i} é um divisor do numero {numero}.");
+                Console.WriteLine("Digite um número para visualizar seus divisores ou 'q' para sair.");
+                var entrada = Console.ReadLine();
+                if (entrada == "q")
+                {
+                    break;
                 }
+
+                try
+                {
+                    var numero = int.Parse(entrada);
+                    Console.WriteLine($"Divisores do numero {entrada}!");
+
+                    for (int i = 1; i <= numero; i++)
+                    {
+                        //verificar se numero é divisor
+                        if (numero % i == 0)
+                        {   
+                            Console.WriteLine($"{i} é um divisor do numero {numero}.");
+                        }
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine($"Erro inesperado: {ex.Message}");
+                    continue;
+                }   
+
             }
-        }
+
+
+        }        
     }
 }
